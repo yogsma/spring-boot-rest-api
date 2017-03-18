f6d1ed9a8d043b54ca8818aaf65ada328073258c
Yogesh Mali <yogesh.mali@gmail.com>
2017-03-18 13:40:00 -0400

    adding swagger documentation

--- Benefits/src/main/java/com/betterjavacode/benefits/Application.java
+++ Benefits/src/main/java/com/betterjavacode/benefits/Application.java
@@ -2,7 +2,18 @@ package com.betterjavacode.benefits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication(scanBasePackages = { "com.betterjavacode.benefits" })
 public class Application {
 
@@ -11,4 +22,23 @@ public class Application {
 
     }
 
+    @Bean
+    public Docket benefitsApi() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Benefits")
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+            .pathMapping("/");
+
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Benefits REST Service")
+            .description(" A simple REST service for Benefits software ")
+            .contact(new Contact("Yogesh Mali", "https://betterjavacode.com", ""))
+            .version("1.0")
+            .build();
+    }
 }
